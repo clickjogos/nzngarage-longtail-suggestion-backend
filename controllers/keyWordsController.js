ca0f70b278bb3473d09cefa3d99b279e658c5c9a
Iago Godoy <iago.lima@ibm.com>
2020-11-25 12:36:16 -0300

    merge fix

--- controllers/keyWordsController.js
+++ controllers/keyWordsController.js
@@ -5,40 +5,6 @@ const { searchKeywordsListByCompetitor } = require('./semRushController')
 
 //getKeyWords - retrieves the key words list from database based on the filtering parameters
 async function getKeyWords(params) {
-<<<<<<< HEAD
-  try {
-
-    let mongoSearchObject = setupMongoFilters(params, "semrush-results")
-
-    //if domain name parameter is sent, set it for the query
-    if (requestParameters.domain) {
-      mongoSearchObject.query["competitor"] = requestParameters.domain
-    }
-
-    let competitors = await mongodbConnector.list(mongoSearchObject)
-
-    var keyWords = {
-      keyWordsArray: []
-    }
-    if (competitors) {
-      competitors.list.map((keywordsObject) => {
-        // delete keywords._id
-        keyWords.keyWordsArray.push(keywordsObject)
-      })
-      return keyWords
-    }
-    else {
-      // keyword search in case we need to return a non-listed competitor
-      // let semRushResults = await searchKeywordsListByCompetitor(params)
-      // keyWords.keyWordsArray.push(semRushResults)
-      return keyWords
-    }
-  }
-  catch (error) {
-    console.log(error)
-    throw new Error(err)
-  }
-=======
 	try {
 		let mongoSearchObject = {
 			collection: 'semrush-results',
@@ -140,130 +106,10 @@ async function getKeyWords(params) {
 		console.log(error)
 		throw new Error(err)
 	}
->>>>>>> 9c8e4962fae9ac357df1b3120a5da2af84ed59a1
 }
 
 // setWeeklySchedule - creates or updates a weekly keyword schedule in the database
 async function setWeeklySchedule(params) {
-<<<<<<< HEAD
-  try {
-
-    //determine current week start date and check if there are existing scheudles
-    let currentDate = new Date();
-    let startDate = new Date()
-    let weekdayNumber = currentDate.getDay()
-    startDate.setDate(currentDate.getDate() - weekdayNumber)
-    startDate.setHours(0, 0, 0, 0);
-
-
-    let plans = await mongodbConnector.list({
-      collection: "week-plans",
-      query: {
-        "weekStartDate": {
-          "$gte": startDate
-        }
-      }
-
-    })
-
-    let scheduleObject;
-    if (plans.list.length > 0) {
-      //update existing schedule
-      scheduleObject = plans.list[0];
-      scheduleObject.scheduledKeywords = []
-    }
-
-    else {
-      //create a new one
-      scheduleObject = {
-        weekStartDate: startDate,
-        lastUpdate: currentDate,
-        scheduledKeywords: []
-      }
-    }
-
-
-    params.selectedKeywords.map((keywordObject) => {
-      keywordObject['title'] = ""
-      keywordObject['tag'] = ""
-      keywordObject['internalBacklinks'] = []
-      scheduleObject.scheduledKeywords.push(keywordObject)
-    })
-
-    //update current week schedule
-    if (scheduleObject._id) {
-      await mongodbConnector.updateOne({
-        collection: "week-plans",
-        filter: {
-          _id: scheduleObject._id
-        },
-        update: {
-          $set: {
-            weekStartDate: scheduleObject.weekStartDate,
-            lastUpdate: currentDate,
-            scheduledKeywords: scheduleObject.scheduledKeywords
-          }
-        }
-      }, true);
-    }
-
-    //create current week schedule
-    else {
-      await mongodbConnector.save({
-        collection: "week-plans",
-        document: scheduleObject
-      });
-    }
-
-    return scheduleObject
-  }
-
-  catch (error) {
-    console.log(error)
-    throw new Error(err)
-  }
-}
-
-
-// retrieveWeeklySchedule - retrieves a weekly keyword schedule in the database the date parameters must be in the format "YYYY-MM-DD" 
-async function retrieveWeeklySchedule(params) {
-  try {
-
-    let mongoSearchObject = setupMongoFilters(params, "week-plans");
-
-    //if the user specify intervals, retrieve every object in that date interval 
-    if (params.startDate && params.endDate) {
-      mongoSearchObject.query['weekStartDate'] = setDateIntervalFilter(params.startDate,params.endDate)
-    }
-    //if date not specified, get for the current week
-    else {
-      let weekStartDate = getCurrentWeekStartDate()
-      mongoSearchObject.query['weekStartDate'] = {
-        "$gte": weekStartDate
-      }
-    }
-
-    //if textSearch parameter is passed, add a filter for keyword and title
-    // if(params.textSearch){
-
-    // }
-
-
-    let plans = await mongodbConnector.list(mongoSearchObject)
-
-    let result = {
-      schedule: plans.list
-    };
-
-    return result
-
-  }
-
-  catch (error) {
-    console.log(error)
-    throw new Error(err)
-  }
-=======
 	try {
 		//determine current week start date and check if there are existing scheudles
 		let currentDate = new Date()
@@ -447,7 +293,6 @@ async function disqualifyKeywords(params) {
 		console.log(error)
 		throw new Error(err)
 	}
->>>>>>> 9c8e4962fae9ac357df1b3120a5da2af84ed59a1
 }
 
 function getCurrentWeekStartDate() {
