6ee6f3c81d8fe7caadc6b8f09714bc0d87aa3178
regiany.nunes@ibm.com <regiany.nunes@ibm.com>
2020-12-02 17:30:07 -0300

    add publish date filter on get audience values

--- controllers/audienceController.js
+++ controllers/audienceController.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 
 const mssqlConnector = require('../connectors/mssqlConnector')
-const { find } = require('../connectors/mongodbConnector')
+const { find, aggregate } = require('../connectors/mongodbConnector')
 const { getDateOnStringFormat } = require('../utils/utils')
 
 async function execute(params) {
@@ -17,12 +17,11 @@ async function execute(params) {
 
 		let audienceValuesGrouppedByIdAndMonth = await grouprawAudienceValuesByIdAndMonth(rawArticlesAudienceValues)
 
-    let audienceAndDetailsValuesGroupped = await groupAudienceAndDetailsValues(audienceValuesGrouppedByIdAndMonth, articles.articlesDetails)
+		let audienceAndDetailsValuesGroupped = await groupAudienceAndDetailsValues(audienceValuesGrouppedByIdAndMonth, articles.articlesDetails)
 
-    let articlesAudienceByMonth = await getAudienceByMonth(audienceValuesGrouppedByIdAndMonth)
-    
-    let tableAndChartInfo = await structureTableAndChartInfo(articles.articlesIdsArray, articlesAudienceByMonth, audienceAndDetailsValuesGroupped)
-    
+		let articlesAudienceByMonth = await getAudienceByMonth(audienceValuesGrouppedByIdAndMonth)
+
+		let tableAndChartInfo = await structureTableAndChartInfo(articles.articlesIdsArray, articlesAudienceByMonth, audienceAndDetailsValuesGroupped)
 
 		return tableAndChartInfo
 	} catch (error) {
@@ -39,16 +38,14 @@ async function defineDateFilters(params) {
 		}
 		if (params.startDate && params.endDate) {
 			dateObject.stringFormat['startDate'] = params.startDate
-      dateObject.stringFormat['endDate'] = params.endDate
+			dateObject.stringFormat['endDate'] = params.endDate
 
-      // let paramsDate = new Date(params.startDate)
-      // dateObject.dateFormat['startDate'] = paramsDate
-      // let weekdayNumber = paramsDate.getDay()
-      // dateObject.dateFormat.startDate.setDate(paramsDate.getDate() - weekdayNumber)
-      // dateObject.dateFormat.startDate.setHours(0, 0, 0, 0)
-
-			dateObject.dateFormat['endDate'] = new Date(params.endDate)
-		} else {
+			dateObject.dateFormat['startDate'] = new Date(params.startDate)
+      dateObject.dateFormat['endDate'] = new Date(params.endDate)
+    }
+    else if ( (!params.startDate && params.endDate) || (params.startDate && !params.endDate) ) {
+      throw new Error("Invalid date parameters. Please make sure to send startDate and endDate values")
+    } else {
 			dateObject.dateFormat['endDate'] = new Date()
 			dateObject.dateFormat['startDate'] = new Date()
 			dateObject.dateFormat.startDate.setDate(dateObject.dateFormat.endDate.getDate())
@@ -62,60 +59,75 @@ async function defineDateFilters(params) {
 		return dateObject
 	} catch (error) {
 		console.error(error)
-		throw error
+		throw error.message
 	}
 }
 
 async function recoverArticlesIds(dateObject) {
 	try {
-		let filterDateQuery = {
-			weekStartDate: {
-				$gte: dateObject.startDate,
-				$lte: dateObject.endDate,
-			},
-			scheduledKeywords: { $exists: true },
-    }
-    
-    // let filterDateQuery = { 
-    //   scheduledKeywords: { $elemMatch: { 'nznPosition': 11  }}
-    // }
 
-		let schedulePlans = await find({
+		let pipelinesStages = [
+			{
+				$unwind: { path: '$scheduledKeywords' },
+			},
+			{
+				$addFields: {
+					'scheduledKeywords.publishDateFormat': {
+						$dateFromString: {
+							dateString: '$scheduledKeywords.publishDate',
+						},
+					},
+				},
+			},
+			{
+				$match: {
+					'scheduledKeywords.publishDateFormat': {
+						$gte: dateObject.startDate,
+						$lte: dateObject.endDate,
+					},
+				},
+			},
+			{
+				$group: {
+					_id: '$_id',
+					scheduledKeywords: { $push: '$scheduledKeywords' },
+				},
+			},
+		]
+
+		let schedulePlans = await aggregate({
 			collection: 'week-plans',
-			query: filterDateQuery,
+			pipelinesStages: pipelinesStages,
 			fieldsToShow: {
-        'scheduledKeywords.articleId': 1,
-        'scheduledKeywords.Keyword': 1,
-        'scheduledKeywords.tag': 1,
-        'scheduledKeywords.cmsLink': 1,
-        'scheduledKeywords.title': 1,
-        'scheduledKeywords.publishDate': 1
+				'scheduledKeywords.articleId': 1,
+				'scheduledKeywords.Keyword': 1,
+				'scheduledKeywords.tag': 1,
+				'scheduledKeywords.cmsLink': 1,
+				'scheduledKeywords.title': 1,
+				'scheduledKeywords.publishDate': 1,
 			},
 		})
 
-    if (schedulePlans.length > 0 ) {
-      let articlesIdsArray = [] // [207361,205760,205970,205176,177783,177216]
-      let articlesDetails = []
-      schedulePlans.map((plan) => {
-        plan.scheduledKeywords.map((keyword) => {
-          if(keyword.articleId) {
-            var articleIdIndex = articlesIdsArray.indexOf(keyword.articleId)
-            if (articleIdIndex === -1) {
-              articlesIdsArray.push(keyword.articleId)
-              articlesDetails.push(keyword)
-            }
-  
-            // articlesIdsArray.push(keyword.articleId) 
-          }
-        })
-      })
-  
-      return { articlesIdsArray, articlesDetails }
-    } else {
-      throw new Error("You don't have any shedule on this date")
-    }
-    
-		// return [207361,205760,205970,205176,177783,177216]
+		if (schedulePlans.length > 0) {
+			let articlesIdsArray = [] // [207361,205760,205970,205176,177783,177216]
+			let articlesDetails = []
+			schedulePlans.map((plan) => {
+				plan.scheduledKeywords.map((keyword) => {
+					if (keyword.articleId) {
+						var articleIdIndex = articlesIdsArray.indexOf(keyword.articleId)
+						if (articleIdIndex === -1) {
+							articlesIdsArray.push(keyword.articleId)
+							articlesDetails.push(keyword)
+						}
+					}
+				})
+			})
+
+			return { articlesIdsArray, articlesDetails }
+		} else {
+			throw new Error("You don't have any article published on this range date")
+		}
+
 	} catch (error) {
 		console.error(error)
 		throw error
@@ -151,25 +163,25 @@ async function grouprawAudienceValuesByIdAndMonth(articlesArray) {
 		let articlesGroupped = _(articlesArray)
 			.groupBy('views_artigo')
 			.map(function (group, key) {
-        let response = { articleId: group[0].views_artigo, monthsAudience: [] }
-        
-        let currentDate = new Date()
-        let last30Days = group.filter( item=> {
-          let itemDate = new Date(item.views_data)
-          let startDate = new Date()
-          startDate.setDate(currentDate.getDate() - 15)
-          if( itemDate > startDate) return item
-        })
+				let response = { articleId: group[0].views_artigo, monthsAudience: [] }
+
+				let currentDate = new Date()
+				let last30Days = group.filter((item) => {
+					let itemDate = new Date(item.views_data)
+					let startDate = new Date()
+					startDate.setDate(currentDate.getDate() - 15)
+					if (itemDate > startDate) return item
+				})
 
 				let months = []
 				group.map((row) => {
 					let date = new Date(row.views_data)
-          let month = date.getMonth() + 1
+					let month = date.getMonth() + 1
 					months.push({ month: month, views_total: row.views_total })
 				})
-        response['monthsAudience'].push(months)
+				response['monthsAudience'].push(months)
 				response['totalAudience'] = _.sumBy(group, 'views_total')
-        response['last30DaysAudience'] = _.sumBy(last30Days, 'views_total')
+				response['last30DaysAudience'] = _.sumBy(last30Days, 'views_total')
 				return response
 			})
 			.value()
@@ -178,16 +190,16 @@ async function grouprawAudienceValuesByIdAndMonth(articlesArray) {
 			let newMonthValues = _(articleGroup.monthsAudience[0])
 				.groupBy('month')
 				.map(function (group, key) {
-          let totalAudience = _.sumBy(group, 'views_total')
+					let totalAudience = _.sumBy(group, 'views_total')
 					return {
 						month: group[0].month,
-            totalMonthAudience: totalAudience
+						totalMonthAudience: totalAudience,
 					}
 				})
 				.value()
 
-      articlesGroupped[articleGroupIndex].monthsAudience = newMonthValues
-      articlesGroupped[articleGroupIndex]['meanAudience'] = articleGroup.totalAudience / articleGroup.monthsAudience.length
+			articlesGroupped[articleGroupIndex].monthsAudience = newMonthValues
+			articlesGroupped[articleGroupIndex]['meanAudience'] = articleGroup.totalAudience / articleGroup.monthsAudience.length
 			return articlesGroupped
 		})
 
@@ -226,67 +238,64 @@ async function getAudienceByMonth(articlesGrouppedById) {
 	}
 }
 
-async function groupAudienceAndDetailsValues(audienceValuesGrouppedByIdAndMonth, articlesDetails){
-  try {
-    
-    let mapArray = [...audienceValuesGrouppedByIdAndMonth, ...articlesDetails]
+async function groupAudienceAndDetailsValues(audienceValuesGrouppedByIdAndMonth, articlesDetails) {
+	try {
+		let mapArray = [...audienceValuesGrouppedByIdAndMonth, ...articlesDetails]
 
-   let group= await  _(mapArray)
-				.groupBy('articleId')
-				.map(function (group, key) {          
-          let responseObject={articleId: parseInt(key)}
-          group.map( gValues =>{
-            let keys = Object.keys(gValues)
-            keys.map( key => {
-              if(key !== 'articleId') responseObject[key] = gValues[key]
-            })
-          })
-					return responseObject
+		let group = await _(mapArray)
+			.groupBy('articleId')
+			.map(function (group, key) {
+				let responseObject = { articleId: parseInt(key) }
+				group.map((gValues) => {
+					let keys = Object.keys(gValues)
+					keys.map((key) => {
+						if (key !== 'articleId') responseObject[key] = gValues[key]
+					})
 				})
-				.value()
-    
-return group
-  } catch (error) {
-    console.error(error)
+				return responseObject
+			})
+			.value()
+
+		return group
+	} catch (error) {
+		console.error(error)
 		throw error
-  }
+	}
 }
 
 async function structureTableAndChartInfo(articlesIds, articlesAudienceByMonth, audienceAndDetailsValuesGroupped) {
 	try {
-    let objectResponse = {
-      "chartInfo": [],
-      "tableInfo": []
-    }
+		let objectResponse = {
+			chartInfo: [],
+			tableInfo: [],
+		}
 
-    let a = articlesIds.map( (articleId, indexArticleId) =>{
-      let findIdMonthValues = audienceAndDetailsValuesGroupped.find( item=> item.articleId === articleId )
-      let keys = Object.keys(findIdMonthValues)
+		let a = articlesIds.map((articleId, indexArticleId) => {
+			let findIdMonthValues = audienceAndDetailsValuesGroupped.find((item) => item.articleId === articleId)
+			let keys = Object.keys(findIdMonthValues)
 
-      let objectToTable = {}
-      keys.map( key =>{
-        objectToTable[key] = findIdMonthValues[key]
-      })
-      objectResponse.tableInfo.push(objectToTable)
+			let objectToTable = {}
+			keys.map((key) => {
+				objectToTable[key] = findIdMonthValues[key]
+			})
+			objectResponse.tableInfo.push(objectToTable)
 
-      let monthValues = findIdMonthValues.monthsAudience
-      let b = monthValues.map( monthValue =>{
-          let findMonth = objectResponse.chartInfo.findIndex( item => item.month === monthValue.month)
-          let findmonth2 = articlesAudienceByMonth.find( item => item.month === monthValue.month)
-          if(findMonth != -1) {
-              objectResponse.chartInfo[findMonth].articles.push(articleId)
-          }
-          else {
-              objectResponse.chartInfo.push({
-                  month: monthValue.month,
-                  articles: [articleId],
-                  totalMonthAudience: findmonth2.totalMonthAudience
-              })
-          }
-      })
-
-  })
-    return objectResponse
+			let monthValues = findIdMonthValues.monthsAudience
+			let b = monthValues.map((monthValue) => {
+				let findMonth = objectResponse.chartInfo.findIndex((item) => item.month === monthValue.month)
+				let findmonth2 = articlesAudienceByMonth.find((item) => item.month === monthValue.month)
+				if (findMonth != -1) {
+					objectResponse.chartInfo[findMonth].articles.push(articleId)
+				} else {
+					objectResponse.chartInfo.push({
+						month: monthValue.month,
+						articles: [articleId],
+						totalMonthAudience: findmonth2.totalMonthAudience,
+					})
+				}
+			})
+		})
+		return objectResponse
 	} catch (error) {
 		console.error(error)
 		throw error
